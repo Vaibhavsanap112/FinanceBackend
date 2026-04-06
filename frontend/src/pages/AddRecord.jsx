c015f94f162354ab4aaff868ef549e3c2a083f9f
Vaibhav <sanapvaibhav8767@gmail.com>
2026-04-06 11:15:26 +0530

    final fix for deployment

--- frontend/src/pages/AddRecord.jsx
+++ frontend/src/pages/AddRecord.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
-import API from "../services/api";
+import API from "../../src/services/api";
 import Navbar from "../components/Navbar";
 
 function AddRecord() {
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import API from "../services/api";
+import API from "../../src/services/api";
 
 function Signup() {
   const navigate = useNavigate();
