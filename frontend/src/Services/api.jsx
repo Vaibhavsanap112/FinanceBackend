83923a10ecfbc22d5cf76e21ff73e5daba4effb8
Vaibhav <sanapvaibhav8767@gmail.com>
2026-04-06 11:06:56 +0530

    final fix for deployment

--- frontend/src/Services/api.jsx
+++ frontend/src/Services/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://financebackend-6x0t.onrender.com/api/v1",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 api.interceptors.request.use((req) => {
@@ -15,3 +15,5 @@ api.interceptors.request.use((req) => {
 });
 
 export default api;
+
+
